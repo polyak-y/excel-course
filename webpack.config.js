71bcbcb809a11194057bd16f4aba02d396662cc5
vpolyakov <polyak-y@yandex.ru>
2020-10-17 17:47:30 +0300

    17.10.2020

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,51 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, 'src'), //указали корневую папку
   mode: 'development',
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js',
+  entry: './index.js', //точка входа
+  output: { //точка выхода
+    filename: 'bundle.[hash].js', //hash - для отмены кэширования
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    }
+  },
+  plugins: [ //плагины
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'src/favicon.ico'), 
+          to:  path.resolve(__dirname, 'dist'), 
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   }
 }
